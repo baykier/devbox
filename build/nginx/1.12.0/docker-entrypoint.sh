2ed4618975ba7c32be095a09d60d8434a249091b
baykier <1035666345@qq.com>
2021-07-07 23:17:16 +0000

    nginx mysql build update

---
+++ build/nginx/1.12.0/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ex
+
+# 设置uid
+if [[ -n "$DEVBOX_UID" && -n "$DEVBOX_GID" ]];then
+    echo "reset www-data uid && gid \n"
+    usermod -u $DEVBOX_UID nginx
+    groupmod -g $DEVBOX_GID nginx
+
+fi
+# boot nginx
+
+nginx -g 'daemon off;'
